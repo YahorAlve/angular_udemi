64391e196cf3ec360bf6e729180478a1b98dd9c0
Egor <egoralve@gmail.com>
2018-11-30 13:40:02 -0600

    porperty binding

--- udemi-class/src/app/server/server.component.ts
+++ udemi-class/src/app/server/server.component.ts
@@ -3,22 +3,21 @@ import { Component } from '@angular/core';
 // export so it could be used in deferent components
 // This class gonna be used like blueprint to create angular objects
 @Component({
-    // here is metadata which tels angular how to use this class
-    // selector name should be uniuqe
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  // here is metadata which tels angular how to use this class
+  // selector name should be uniuqe
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  // We can just assign values w/o tipe
+  serverId = 10;
+  serverStatus = 'Offline';
 
-    // We can just assign values w/o tipe
-    serverId = 10;
-    serverStatus = 'Offline';
+  // but also can use typescript feature to assign specific types
+  // serverId: number = 10;
+  // serverStatus: string = 'Offline';
 
-    // but also can use typescript feature to assign specific types
-    // serverId: number = 10;
-    // serverStatus: string = 'Offline';
-
-    getServerStatus() {
-        return this.serverStatus;
-    }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- udemi-class/src/app/servers/servers.component.ts
+++ udemi-class/src/app/servers/servers.component.ts
@@ -6,17 +6,27 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // - selector
   // selector: '.app-servers', - through class
   // whether templateUrl or template must be presented in declaration. Each component must have template
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
-  template: `
+  /* template: `
     <app-server></app-server>
     <app-server></app-server>
-  `,
+  `, */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  addNewServerAllowed = false;
+
+  // constructor is method executed at time of component created by the angular
+  constructor() {
+    // setTimeout - regular javascript function
+    // since ES6 instead of function() {} we can type () => {}. And note this will now work in function() sintacsis
+    setTimeout(() => {
+      this.addNewServerAllowed = true;
+    },
+      10000);
+  }
 
   ngOnInit() {
   }
