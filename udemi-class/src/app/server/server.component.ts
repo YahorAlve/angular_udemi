22fe320db59eb294fe6c4a163d6a2e23f93ac3d2
Egor <egoralve@gmail.com>
2018-12-11 17:52:30 -0600

    ngStyle and ngClass directives

--- udemi-class/src/app/server/server.component.ts
+++ udemi-class/src/app/server/server.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 // export so it could be used in deferent components
 // This class gonna be used like blueprint to create angular objects
 @Component({
-  // here is metadata which tels angular how to use this class
+  // here is metadata which tells angular how to use this class
   // selector name should be uniuqe
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   // We can just assign values w/o tipe
@@ -17,7 +22,16 @@ export class ServerComponent {
   // serverId: number = 10;
   // serverStatus: string = 'Offline';
 
+  // same as java called only once by creation of component - each selector created in html is standalone component
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
