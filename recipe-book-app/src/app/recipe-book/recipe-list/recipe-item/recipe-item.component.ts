c4fb806256c29ecdd9f72278fd2519c64f92f5f5
Egor <egoralve@gmail.com>
2018-12-20 17:21:43 -0600

    Recipe beggining

---
+++ recipe-book-app/src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.module';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+  // assing array type to property for typescript we can by name: type[]
+  recipies: Recipe[] = [
+    new Recipe('Test Name', 'Test Description', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ recipe-book-app/src/app/recipe-book/recipe.module.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+    // that is how types assigned to properties -> name: type;
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
