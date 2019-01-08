edb6fb1291bce03d54644dce1986480292278c7a
Egor <egoralve@gmail.com>
2019-01-08 12:22:38 -0600

    Local references through ViewChild Decorator

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,7 +10,16 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  /*
+  Another approach to reach HTML elment from DOM using angular is through ElementRef in ts code using
+  ViewCHild decorator. That will not be HTMLInputElement - it is going to be ElmentRef element wich is
+  Angular object core object - but through it we can access native element. It is strongly recommende do
+  not modify value of native dom element through directly accessing it through angular like this :
+  this.serverContentElement.nativeElement.value = 'something' cause it is actlialy anti-pattern and could 
+  lead to bad consecuences.
+   */
+  @ViewChild('serverContentElement') serverContentElement: ElementRef;
 
   constructor() { }
 
@@ -20,14 +29,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(serverNameElement: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverNameElement.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentElement.nativeElement.value
     });
   }
 
   onAddBlueprint(serverNameElement: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: serverNameElement.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentElement.nativeElement.value
     });
   }
 
