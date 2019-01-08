4058b1b41954f1f4dae4d1d94a0de8ec4e375f2b
Egor <egoralve@gmail.com>
2019-01-08 11:22:22 -0600

    View Encapsulation - None (Global CSS), Emulated, Native

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None - will aply component CSS file globaly to all elements even though they have angular attributes
+  // Emulated is default
+  // Native will be behaviour like for emulated but only for browsers which support it
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
 
