a433e70abdd01dc8076ac68636b912cb8fd08d30
Egor <egoralve@gmail.com>
2019-01-11 11:46:04 -0600

    Section 6  - show recipe or shopping list ngIf with Output event binding

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() showRecipe = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showRecipes(show: boolean) {
+    console.log('Show recipe : ' + show);
+    this.showRecipe.emit(show);
   }
 
 }
