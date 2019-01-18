6e66ca240de930fabc2ce39ae55adab0e0063b53
Egor <egoralve@gmail.com>
2019-01-17 20:14:24 -0600

    Section 7 Task own directive dropdown lector solution - much better

--- recipe-book-app/src/app/shared/dropdown.directive.ts
+++ recipe-book-app/src/app/shared/dropdown.directive.ts
@@ -1,23 +1,17 @@
-import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit {
 
-  isOpen = false;
+  @HostBinding('class.open') isOpen = false;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {}
 
   @HostListener('click') openOnClick() {
-    if (this.isOpen) {
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-      this.isOpen = false;
-    } else {
-      this.renderer.addClass(this.elementRef.nativeElement, 'open');
-      this.isOpen = true;
-    }
+    this.isOpen = !this.isOpen;
   }
 }
