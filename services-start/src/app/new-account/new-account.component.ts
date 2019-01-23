e623638abb7a369def1fb7dcfbca706b715719b1
Egor <egoralve@gmail.com>
2019-01-22 20:40:34 -0600

    Section 9 Fixing accountService Hierarchial Injector

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -6,7 +6,11 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  // if we put here service we tell angular create new service for this module and each child of this component
+  // in case child component doesn't declare its own service instance (object)
+  // after we removed AccountService we started using appComponent AccountService instance and appliaction will work fine
+  // as we will add accounts in the object appCOmponent pointed to and uses for reading values
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
