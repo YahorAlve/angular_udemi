9475d695c496c3916d0f635f323e70890d469c29
Egor <egoralve@gmail.com>
2019-01-22 20:26:24 -0600

    Section  9 AccountService but with some issues (will be fixed next commit)

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,23 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   // right way to inject services into componenet through constructor - ang will take care for injecting the service
-  constructor(private log: LoggingService) {}
+  constructor(private log: LoggingService,
+              private accountService: AccountService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
     this.log.logStatusChanged(accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
