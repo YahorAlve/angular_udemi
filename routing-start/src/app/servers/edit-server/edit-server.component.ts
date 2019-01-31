234bb2cac842ce590542ad4a0a5924b8709b3e1e
Egor <egoralve@gmail.com>
2019-01-31 14:19:52 -0600

    Section 11 Retrieving query params and fragments in component

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,17 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // same like previous commit that is going work only one time when component is initialized
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    // if we want to have it updated each time url changed even when we do it from initialized component itself
+    // we need to subscribe to obsevable (event)
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
