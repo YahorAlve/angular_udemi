98c9073c29c76114c0869ca5484098c970ee149e
Egor <egoralve@gmail.com>
2019-02-08 16:50:34 -0600

    Section 13 Custom Observable from scratch

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import 'rxjs/Rx';
 
 @Component({
@@ -12,12 +12,36 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(2000);
+    /* const myNumbers = Observable.interval(2000);
     myNumbers.subscribe(
       (number: number) => {
         console.log(number);
       }
-    );
+    ); */
+    /* Creating new Observable which will fire in 1 and fail in 2 and complete in 3. Create accept function as an argument
+     which is supposed to be our asynch code.*/
+     /* This observer is just used to build up observable to genreate data packages and is not the same as observer (subscriber) outside */
+    const myObservable = Observable.create((observer: Observer<string>) => {
+        setTimeout( () => {
+          observer.next('First Value');
+        }, 1000);
+        setTimeout( () => {
+          // observer.error('Error Value');
+        }, 2000);
+        setTimeout( () => {
+          observer.complete();
+        }, 3000);
+        setTimeout( () => {
+          observer.next('Last Value - wont be fired as observer will be completed');
+        }, 4000);
+      });
+      /* 3 functions : 1 for data, second for error, 3rd for complete event processing. If there was an error
+      our observer won;t catch completed event. So one onle coudl be whether Completed or Error. */
+      myObservable.subscribe(
+        (data: string) => { console.log(data); },
+        (error: string) => { console.log(error); },
+        () => { console.log('Completed'); }
+      );
   }
 
 }
