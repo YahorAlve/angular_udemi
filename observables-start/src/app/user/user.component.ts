26d992a2baf700d222ec9f566007d0ae187282b7
Egor <egoralve@gmail.com>
2019-02-08 15:18:32 -0600

    Section 13 Small comment and creation simpliest observeable (interval)

--- observables-start/src/app/user/user.component.ts
+++ observables-start/src/app/user/user.component.ts
@@ -12,6 +12,9 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /* This is an observer (subscriber) and observable is params Data source and it has anonymus function subscribe which
+       accepts functions as parametrs. And it can accept 3 function : 1 one for working with data comes back, second
+       for work with an error (error handeling), third one is completion of observable (e.g. http request observable) */
     this.route.params
       .subscribe(
         (params: Params) => {
