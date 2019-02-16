e53d44ba8925dcbda9e8b3a0449665d35b2ff976
Egor <egoralve@gmail.com>
2019-02-15 18:02:36 -0600

    Section 15 Practive add a few fixes

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+@ViewChild('myForm') myForm: NgForm;
+
+onSubmit() {
+  console.log(this.myForm.value);
+}
 
 }
