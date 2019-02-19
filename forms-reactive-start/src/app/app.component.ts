e8814ca3a9c5d4e332c2cd4b52f09f0799978cbe
Egor <egoralve@gmail.com>
2019-02-19 12:58:07 -0600

    Section 15 Reactive forms custom validators (+ note of this.)

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  forbiddenUserNames = ['Chris', 'Anna'];
 
   /* In TD approach we had NgFrom wich angular papulated automatically and NgForm is actially wrapper to FormGroup
      and FormGroup it is just a gropu of controllers.*/
@@ -18,7 +19,14 @@ export class AppComponent implements OnInit {
     // this new FormGroup({}); will just create empty form wiht nothing
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username' : new FormControl(null, Validators.required),
+        /* Mentor mentioned a few times that function name w/o () it is just refernce to function, he mentioend don't 
+        execute it just pass the reference (w/o parentheses)*/
+        /* IF we just pass this.forbiddenNames it will fail with tricky exception as inside function forbiddenNames we 
+        have this.forbiddenUserNames - and based on mentor comment it is javascript specific and it is improtant who is
+        going to call this fucntion. In our case it is not called in this specific class it is called outside in some angular 
+        form class and it doesn't have such property as this.forbiddenUserNames. But we can add binding to our current class 
+        this as we did below and it will find this.forbiddenUserNames as this will be pointed to this currecnt class. */
+        'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -34,5 +42,14 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     // example of casting to FormArray for typescript not throwing an error
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  /* This is just a syntax {[s:string]: boolean } saying that it will return json where key is string and value is boolean */
+  forbiddenNames(control: FormControl): {[s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true };
+    }
+    /* If validation passed successfully we should return nothing or null. We should not return {'key': false} */
+    return null;
   }
 }
