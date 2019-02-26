436f1e3edd2c347484384dd021f0c4a0690b8989
Egor <egoralve@gmail.com>
2019-02-26 14:10:16 -0600

    Section 16 Forms Practice Project - Reactive approach creating Recipe form in code

--- recipe-book-app/src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ recipe-book-app/src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,8 +13,9 @@ export class RecipeEditComponent implements OnInit {
 
   name: string;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {
   }
 
   ngOnInit() {
@@ -20,8 +24,27 @@ export class RecipeEditComponent implements OnInit {
         this.name = params['name'];
         this.editMode = params['name'] != null;
         console.log(this.editMode);
+        this.initForm();
       }
     );
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeByName(this.name);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'descritption': new FormControl(recipeDescription),
+    });
   }
 
 }
