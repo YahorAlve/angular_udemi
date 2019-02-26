7e5faed0d9e2945d2dd0c22b55c82b1a0bb8c448
Egor <egoralve@gmail.com>
2019-02-26 10:21:15 -0600

    Section 16 Forms Practice Project - Replacing local reference to TD form approach

--- recipe-book-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { IngredientModule } from 'src/app/shared/ingredient/ingredient.module';
 import { ShoppinListService } from '../shoppin-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -14,10 +15,11 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(nameInput: HTMLInputElement, amountInput: HTMLInputElement) {
+  onSubmit(form: NgForm) {
     // diff between const and let that const can be assigned only one and can't be re-assigned, let - can be
     // const ingredient = new IngredientModule(nameInput.value, +amountInput.value);
-    this.shoppingListService.addNewIngredient(new IngredientModule(nameInput.value, +amountInput.value));
+    const value = form.value;
+    this.shoppingListService.addNewIngredient(new IngredientModule(value.name, value.amount));
   }
 
 }
