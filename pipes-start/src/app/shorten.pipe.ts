d5ad7cf8ab9ccaf6df2b7e56467172aa8cc8053f
Egor <egoralve@gmail.com>
2019-02-28 15:01:48 -0600

    Section 17 Pipes - Creating Filter Pipe

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -6,8 +6,8 @@ import { PipeTransform, Pipe } from '@angular/core';
 export class ShortenPipe implements PipeTransform {
 
 
-    /* If we want more params we just add new ones. Also pipe w/o parametr will call this function 
-    with param null here as well */
+    /* If we want more params we just add new ones. Also pipe w/o parametr will call this function
+    with param null here as well. */
     transform(value: any, limit: number) {
         if (value.length > limit ) {
             return value.substr(0, limit) + ' ...';
