17e31dfa12cb602aec6966bb3f2ff14ba03ea424
Egor <egoralve@gmail.com>
2019-02-28 18:04:06 -0600

    section 17 Pipe - Sort and Revert Pipes practive

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
 import { FilterPipe } from './filter.pipe';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
 
 /* Same as components and directives we need to add pipes onto declarations */
 @NgModule({
   declarations: [
     AppComponent,
     ShortenPipe,
-    FilterPipe
+    FilterPipe,
+    ReversePipe,
+    SortPipe
   ],
   imports: [
     BrowserModule,
