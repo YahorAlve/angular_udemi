21373bfaac2da5797c36739d1d2b74e46c38f511
Egor <egoralve@gmail.com>
2019-03-08 17:36:26 -0600

    Section 20 Auth - setting up firebase

---
+++ recipe-book-app/src/app/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  signUpUser(email: string, password: string) {
+
+  }
+}
