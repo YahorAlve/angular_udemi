9685afa1e23317b070f5903a1d5f3ede31e0cc17
Egor <egoralve@gmail.com>
2019-03-08 17:54:40 -0600

    Section 20 Auth - Signing user Up with Firebase

--- recipe-book-app/src/app/auth/auth.service.ts
+++ recipe-book-app/src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,9 @@ export class AuthService {
   constructor() { }
 
   signUpUser(email: string, password: string) {
-
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(
+          error => console.log(error)
+        );
   }
 }
