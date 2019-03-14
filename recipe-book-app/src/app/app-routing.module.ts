5db737d490a117f77f8074d73a8193e8bebe38d9
Egor <egoralve@gmail.com>
2019-03-14 13:20:28 -0500

    Section 21 Modules - Creation of Shopping List module and a few important notes.

--- recipe-book-app/src/app/app-routing.module.ts
+++ recipe-book-app/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   /* WE need to be very carful with order we put our paths. E.g. if we put :name and then new each time
     we have in url /new it will be first considered as :id and will be an error */
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  /* For routing it is not important to have component and routing module in the same direcotiry, it is just important
+  that we declare this component somewhere before it gets used.
+  But if we use selector in html like app-shoppinglist-component in app-component it will fail as it is requered to have it declared or
+  imported through other module which exports this component.*/
   { path: 'shoppingList', component: ShoppingListComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -1,44 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PleaseSelectComponent } from './please-select/please-select.component';
 import { HttpModule } from '@angular/http';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { RecipesModule } from './recipe-book/recipes.module';
 import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   /* In declarations we put Components, Directives and Pipes the module uses. */
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     PageNotFoundComponent,
     SignupComponent,
     SigninComponent
   ],
-  /* In imports modules which current module is using. From importated modules will be only importated stuff which is 
+  /* In imports modules which current module is using. From importated modules will be only importated stuff which is
   exported. E.g. FormsModule has exported form directives (@NgForm) we can use after importing this FormsModule. */
   imports: [
     BrowserModule,
-    FormsModule,
+    /* We moved FormsModule as directives exposed by this module only used in shoppingListModule. But be aware that
+    mentor mentioned stuff exposed(exported) by Module are avalable in modules directly importing them. So if we want to use 
+    directives of FormsModule in appModule and shoppinListModule we need to import them in both places.*/
+    /* Actially FormsModule is used in SignUp SignIn forms as well and application will fail as this is not imported in app module anymore
+    but we can export FormsModule in ShoppingModule (not best practice) or create separate AuthModule and import it there. */
     /* There is some importance to have RecipesModule (which includes RecipeRouting Modules) before AppRoutingModule for working
-    wildCards correctly. But was not clear why just looks some specific how angular builds/maps routing pathes while injecting child 
+    wildCards correctly. But was not clear why just looks some specific how angular builds/maps routing pathes while injecting child
     modules. */
     RecipesModule,
     AppRoutingModule,
     HttpModule,
-    SharedModule
+    SharedModule,
+    ShoppingListModule
   ],
   /* Providrs we already know if put below it one same instnace will be shared among whole application. I am unsing anatation in root.*/
   /* But we still leave RecipeService in root as it is used amon other components - not only Recipe Feature.
