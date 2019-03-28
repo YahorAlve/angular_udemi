1d68a1082350fade232072d463c711f09a99c80c
Egor <egoralve@gmail.com>
2019-03-28 16:27:38 -0500

    Section 24 NGRX - add authentication

--- recipe-book-app/src/app/core/header/header.component.ts
+++ recipe-book-app/src/app/core/header/header.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import * as fromApp from '../../store/app.reducer';
+import * as fromAuth from '../../auth/store/auth.reducer';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +14,14 @@ import { HttpEvent, HttpEventType } from '@angular/common/http';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  authState: Observable<fromAuth.State>;
 
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState = this.store.select('auth');
   }
 
   onSaveData() {
