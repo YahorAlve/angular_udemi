04b98a2159f0aad01a8cef7c16c3d63c900b2b9a
Egor <egoralve@gmail.com>
2019-03-30 10:58:34 -0500

    Section 24 NGRX - Changing to Auth store - map, switch map usage

--- recipe-book-app/src/app/auth/auth-guard.service.ts
+++ recipe-book-app/src/app/auth/auth-guard.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { AuthService } from './auth.service';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as fromAuth from '../auth/store/auth.reducer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    return this.authService.isAuthenticated();
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.store.select('auth').pipe(map(
+        (stateAuth: fromAuth.State) => {
+          return stateAuth.authenticated;
+        }
+      )
+    );
   }
 }
--- recipe-book-app/src/app/shared/auth.interceptor.ts
+++ recipe-book-app/src/app/shared/auth.interceptor.ts
@@ -1,11 +1,15 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as fromAuth from '../auth/store/auth.reducer';
+import { map, switchMap } from 'rxjs/operators';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService) {}
+    constructor(private store: Store<fromApp.AppState>) {}
     /* All requests are imutable. That is made to avoid midifying of request twice when we are trying to rerty the same
     request*/
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -13,7 +17,11 @@ export class AuthInterceptor implements HttpInterceptor {
         /* const cloneRec = req.clone(); will still return clone which is imutable (can't change there), but we can
         provide json to clone function which will override some params in new clonned object and return it with new ones
          */
-        const clonedRec = req.clone({params: req.params.set('auth', this.authService.getTokenId())});
-        return next.handle(clonedRec);
+        return this.store.select('auth').pipe(switchMap(
+            (authState: fromAuth.State) => {
+                const clonedRec = req.clone({params: req.params.set('auth', authState.token)});
+                return next.handle(clonedRec);
+            }
+        ));
     }
 }
