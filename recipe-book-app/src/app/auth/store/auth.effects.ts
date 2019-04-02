01ce4d1a9667819c814048e3aa25dde8935bd599
Egor <egoralve@gmail.com>
2019-04-02 13:12:17 -0500

    Section 24 NGRX - Sign Up effects

--- recipe-book-app/src/app/auth/store/auth.effects.ts
+++ recipe-book-app/src/app/auth/store/auth.effects.ts
@@ -1,12 +1,43 @@
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+import * as firebase from 'firebase';
 
+import { from } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class AuthEffects {
+
+    /* The main idea behind effects that we are doing something after event happend but we don't change the state at all*/
 
     /* Effect will execute code only when ofType event occurs(dispatched) from anywhere in application */
     @Effect()
     authSignUp = this.actions$.pipe(
-        ofType(AuthActions.TRY_SIGN_UP)
+        ofType(AuthActions.TRY_SIGN_UP),
+        map((action: AuthActions.TrySignUp) => {
+            return action.payload;
+        }),
+        switchMap((user: {username: string, password: string}) => {
+            return from(firebase.auth().createUserWithEmailAndPassword(user.username, user.password));
+        }),
+        switchMap(() => {
+            return from(firebase.auth().currentUser.getIdToken());
+        }),
+        // it is like map but for all events, so here we are going to emit 2 observables
+        // https://medium.com/@shairez/a-super-ninja-trick-to-learn-rxjss-switchmap-mergemap-concatmap-and-exhaustmap-forever-88e178a75f1b
+        // We must dispatch in the end actions or if we don't to add @Effect({dispatch: false})
+        mergeMap((token: string) => {
+            return [
+                {
+                    type: AuthActions.SIGN_UP
+                },
+                {
+                    type: AuthActions.SET_TOKEN,
+                    token: token
+                }
+            ];
+        })
     );
 
     /* Observable Name convetnion is add $ to the name like name$ */
