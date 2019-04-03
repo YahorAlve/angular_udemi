7bc959c64d04eb1960225d47814283adb110fc9f
Egor <egoralve@gmail.com>
2019-04-03 18:02:00 -0500

    Section 24 NGRX -  Recipe List consuming state of Recipes

--- recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,35 +1,26 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
-import { Recipe } from '../recipe.module';
-import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import * as fromRecipe from '../store/recipe.reducer';
+import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit, OnDestroy {
+export class RecipeListComponent implements OnInit {
 
   // assing array type to property for typescript we can by name: type[]
-  recipes: Recipe[];
-  subscription: Subscription;
+  recipeState: Observable<fromRecipe.State>;
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private store: Store<fromRecipe.FeatureState>) {
   }
 
   ngOnInit() {
-    this.recipes = this.recipeService.getRecipes();
-    this.subscription = this.recipeService.recipeChanged.subscribe(
-      (recipes: Recipe[]) => {
-        this.recipes = recipes;
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.recipeState = this.store.select('recipes');
   }
 
   addNewRecipe() {
